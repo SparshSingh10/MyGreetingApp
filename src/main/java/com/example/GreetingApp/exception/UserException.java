df176be49f459fdb356ab56123e7d147cb3d03c8
SparshSingh10 <sparsh262002@gmail.com>
2025-03-05 16:28:00 +0530

    first commit

--- src/main/java/com/example/GreetingApp/exception/UserException.java
+++ src/main/java/com/example/GreetingApp/exception/UserException.java
@@ -1,12 +1,16 @@
 package com.example.GreetingApp.exception;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@AllArgsConstructor
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserException extends Exception {
+    private final String message;
 
-public class UserException extends RuntimeException {
-
-
-        public UserException(String message) {
-            super(message);
-        }
-
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
